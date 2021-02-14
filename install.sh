21d4e92fef533810123b141e89bf65901b8427c9
Pratham Singh <prathamsingh212@gmail.com>
2021-02-14 12:21:21 +0530

    Nodejs Update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt update -y
-sudo apt install -y nodejs
-sudo apt install -y npm
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt -y install nodejs
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt install -y mongodb-org
