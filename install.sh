7b95d9dede5148ed4be429ae46a64520af988a6e
Pratham Singh <prathamsingh212@gmail.com>
2021-02-14 11:38:17 +0530

    Script Automation

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-sudo apt update
-sudo apt install nodejs
-sudo apt install npm
+sudo apt update -y
+sudo apt install -y nodejs
+sudo apt install -y npm
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt install -y mongodb-org
